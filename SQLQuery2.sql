0e1ac72beec6ebfb8ec79448205b479bc5246e6e
Faruk <farukomererdem2003@gmail.com>
2024-05-13 11:40:50 +0000

    Update

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -39,3 +39,34 @@ BEGIN
 	IF (@mailCount!=0) SELECT @sonuc=0
 	RETURN @sonuc
 END
+
+
+CREATE PROCEDURE Kitaplar_ReadAll
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT * FROM Kitaplar;
+END
+
+
+EXEC Kitaplar_ReadAll
+
+CREATE PROCEDURE ekleKullanici
+(@ad VARCHAR(50), @soyad VARCHAR(50), @email VARCHAR(50), @adres NTEXT, @telno CHAR(15), @password CHAR(10))
+AS BEGIN
+	INSERT INTO Kullanici(ad,soyad,email,adres,telno,password) VALUES(@ad,@soyad,@email,@adres,@telno,@password)
+END
+
+EXEC ekleKullanici @ad='Faruk',@soyad='ERDEM',@email='omer.erdem@gmail.com',@adres='Istanbul',@telno='12345678912345',@password='omer123'
+
+CREATE PROCEDURE getirTumKullanicilar
+AS BEGIN
+	SELECT * FROM Kullanici
+END
+
+ALTER PROCEDURE getirTumKullanicilar
+AS BEGIN
+	SELECT * FROM Kullanici ORDER BY id DESC
+END
+
+EXEC getirTumKullanicilar
